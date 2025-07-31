6d300336c9f1b49b85d402e139bb3eb159f27321
Lucca Chantre <luccachantre@gmail.com>
2025-07-30 22:49:18 -0400

    create react app, clean up template code and add component files

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+//"Tip for you guys following along in 2024. Fragments is no longer needed in react. 
+//You can just use empty elements "<></>" instead of a fragment now." - a youtube comment
+
+function App() {
+  return (
+    <div></div>
+  );
+}
+
+export default App;
