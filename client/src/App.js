400fb2dfb762eabfca39ac569415ad7d082307f0
Lucca Chantre <luccachantre@gmail.com>
2025-07-31 22:04:55 -0400

    added very simple h1 tag in input component, imported it to app.js and got the h1 displaying in the app when we run npm start!

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
+//import React, { Fragment } from "react"; //deprecated I believe
 import './App.css';
 //"Tip for you guys following along in 2024. Fragments is no longer needed in react. 
 //You can just use empty elements "<></>" instead of a fragment now." - a youtube comment
 
+//components
+import InputTodo from "./components/InputTodo";
+
 function App() {
   return (
-    <div></div>
+    <div>
+      <InputTodo></InputTodo>
+    </div>
   );
 }
 
