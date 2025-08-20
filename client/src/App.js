fde53362989b022b11ee9e96b7c6b6ea69d43253
Lucca Chantre <luccachantre@gmail.com>
2025-08-20 19:13:55 -0400

    made some changes to InputTodo component, learned that bootstrap works by setting classNames to certain keywords to apply styles directly to elements

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-//import React, { Fragment } from "react"; //deprecated I believe
+import React, { Fragment } from "react"; //deprecated I believe
 import './App.css';
 //"Tip for you guys following along in 2024. Fragments is no longer needed in react. 
 //You can just use empty elements "<></>" instead of a fragment now." - a youtube comment
@@ -8,9 +8,9 @@ import InputTodo from "./components/InputTodo";
 
 function App() {
   return (
-    <div>
+    <Fragment>
       <InputTodo></InputTodo>
-    </div>
+    </Fragment>
   );
 }
 
