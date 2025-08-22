cd1d83e78f269c50577b0c0cdbe7c4bbee479971
Lucca Chantre <luccachantre@gmail.com>
2025-08-21 20:32:25 -0400

    just trying to sync with main

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -4,7 +4,7 @@ const InputTodo = () => {
     return (
         <Fragment>  
             <h1 className="text-center mt-5">Input Todo</h1>
-            <form className="d-flex">
+            <form className="d-flex mt-5">
                 <input type="text" className="form-control"/>
                 <button className="btn btn-success">Add</button>
             </form>
