69b8be6fb9dd14d87aab0f4828a3bc5449fa1012
Lucca Chantre <luccachantre@gmail.com>
2025-08-26 18:09:01 -0400

    this commit is only changine the onSubmitForm function's console.log(response) statement to a window.location = / statement, so that when we submit the input text goes away and doesnt just stay there, which may confuse users as to if them submitting actually did anything

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -14,7 +14,8 @@ const InputTodo = () => {
                 body: JSON.stringify(body)
             });
 
-            console.log(response);
+            //console.log(response);
+            window.location = "/";
         } catch (err) {
             console.error(err.message);
         }
