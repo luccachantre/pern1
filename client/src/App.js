0246ad85e4919451941638188bca3eec2c9335b6
Lucca Chantre <luccachantre@gmail.com>
2025-08-26 18:12:54 -0400

    created a starting shell of the listTodos component. I originally made these changes on input-component branch by accident, I forgot to switch to a new one, but I was able to fix it by doing git stash, removing all changes on the current (wrong) branch, then creating and switching to this branch, and finally doing git stash pop and all the changes reappeared on the correct branch so I could then add commit and push

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,13 @@ import './App.css';
 
 //components
 import InputTodo from "./components/InputTodo";
+import ListTodos from "./components/ListTodos";
 
 function App() {
   return (
     <Fragment>
       <InputTodo></InputTodo>
+      <ListTodos></ListTodos>
     </Fragment>
   );
 }
