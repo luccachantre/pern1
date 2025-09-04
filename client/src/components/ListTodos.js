fb8288ff0483d6cf51abd620de86802ad5292976
Lucca Chantre <luccachantre@gmail.com>
2025-09-03 21:28:51 -0400

    added fragment import and added table template to listTodos component

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const ListTodos = () => {
-    return <h1>List Todos</h1>;;
+    return <Fragment>
+        <table class="table table-dark table-striped mt-5 text-center">
+            <thead>
+            <tr>
+                <th>Firstname</th>
+                <th>Lastname</th>
+                <th>Email</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>John</td>
+                <td>Doe</td>
+                <td>john@example.com</td>
+            </tr>
+            <tr>
+                <td>Mary</td>
+                <td>Moe</td>
+                <td>mary@example.com</td>
+            </tr>
+            <tr>
+                <td>July</td>
+                <td>Dooley</td>
+                <td>july@example.com</td>
+            </tr>
+            </tbody>
+        </table>
+    </Fragment>;
 }
 
 export default ListTodos;
