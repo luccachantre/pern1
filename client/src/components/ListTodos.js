dad8a00e7abb6591b2bdfd468feb14d57cbd06ba
Lucca Chantre <luccachantre@gmail.com>
2025-09-16 17:47:50 -0400

    completed the deleteTodo function to the point that it deletes from the database, but doesnt update the page to reflect it instantly, the page still needs to be reloaded to reflect the database change. next time we will implement that logic

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -6,7 +6,10 @@ const ListTodos = () => {
 
     const deleteTodo = async (id) => {
         try {
-            const deleteTodo = await fetch(`http://local:5000/todos/${id}`)
+            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+                method: "DELETE"
+            });
+            console.log(deleteTodo);
         } catch (err) {
             console.error(err.message);
         }
