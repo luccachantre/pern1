cd8995769cc0c9fa4693ba6b8e2a68817fd561be
Lucca Chantre <luccachantre@gmail.com>
2025-09-23 19:19:26 -0400

    feels like I lost progress. the bootstrap 4 modal doesnt work, and I tried importing a bunch of stuff and updating react and installing react bootstrap and trying that modal template and it also didn't work so now I dont know what to do. If anything react-bootstrap template caused more issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Fragment } from "react"; //deprecated I believe
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 //"Tip for you guys following along in 2024. Fragments is no longer needed in react. 
 //You can just use empty elements "<></>" instead of a fragment now." - a youtube comment
 
--- client/src/components/EditTodos.js
+++ client/src/components/EditTodos.js
@@ -1,33 +1,39 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 const EditTodo = () => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
         <Fragment>
-            <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#myModal">
-            Edit
-            </button>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+  Open modal
+</button>
 
-            <div class="modal" id="myModal">
-            <div class="modal-dialog">
-                <div class="modal-content">
+<div class="modal" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
 
-                <div class="modal-header">
-                    <h4 class="modal-title">Edit Todo</h4>
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                </div>
+      <div class="modal-header">
+        <h4 class="modal-title">Modal Heading</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
 
-                <div class="modal-body">
-                    <input type="text" className="form-control" />
-                </div>
+      <div class="modal-body">
+        Modal body..
+      </div>
 
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-warning" data-dismiss="modal">Edit</button>
-                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+      </div>
 
-                </div>
-            </div>
-            </div>
+    </div>
+  </div>
+</div>
         </Fragment>
     )
 }
